ee5ff407fa09c52eb02812398079192e2fbf2842
Seth Gray <sethgray26@yahoo.com>
2019-02-14 12:30:23 -0700

    signup and login page

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import flickrLogo from '../../photos/flickrLogo.svg'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import './Signin.css'
 
 export default class Signup extends Component {
     constructor() {
@@ -8,11 +13,37 @@ export default class Signup extends Component {
         }
     }
 
-    render(){
-        return(
-            <h1>
-                Signin!
-            </h1>
+    render() {
+        return (
+            <div className='signin-page'>
+                <div className='black-bar-top'>
+                    <img src={flickrLogo} className='flickrLogo'></img>
+                </div>
+                <div className='login-card'>
+                    <div className='two-circles'>
+                        <div className='blue-circle'></div>
+                        <div className='pink-circle'></div>
+                    </div>
+                    <h3> Log in to Flickr </h3>
+                    <h4 className='login-top-text'> We're updating how Flickr members
+                    <h4 className='login-middle-text'>log in. You'll still use your Yahoo login </h4>
+                        <h4 className='login-bottom-text'> until further notice.
+                     </h4>
+                    </h4>
+                    <TextField
+                        id="outlined-email-input"
+                        label="Email address"
+                        type="email"
+                        name="email"
+                        autoComplete="email"
+                        margin="normal"
+                        variant="outlined"
+                        style={{ marginTop: -3, width: 268 }}
+                    />
+                    <Button className='signin-continue-btn' variant='contained' color='primary' component={Link} to='HomePage' style={{ marginTop: 20, width: 268, height: 40 }}> Next </Button>
+                    <h4 className='login-signup-link'> Not a flickr member? <Link to='Signup'> Sign up here. </Link> </h4>
+                </div>
+            </div>
         )
     }
 }
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button'
 import './Signup.css'
 import axios from 'axios';
 
@@ -40,79 +42,87 @@ class Signup extends Component {
 
     async register() {
         const { first_name, last_name, email, password } = this.state
-        const res = await axios.post('/Signup', { first_name: first_name, last_name: last_name, email: email, password: password })
+        const res = await axios.post('/api/register', { first_name: first_name, last_name: last_name, email: email, password: password })
         if (res.data.loggedIn) {
             this.props.history.push('/HomePage')
         }
-        this.props.getUserData(res.data.userData.first_name, res.data.userData.last_name, res.data.userData.profile_pic, res.data.userData.email)
+        // this.props.getUserData(res.data.userData.first_name, res.data.userData.last_name, res.data.userData.profile_pic, res.data.userData.email)
     }
 
     render() {
-        const { classes } = this.props
         return (
             <div className='Signup-background'>
 
                 <div className='registration-box'>
+                    <div className='signup-top'>
+                        <h3> Sign Up</h3>
+                        <p> Use your current email address </p>
+                    </div>
                     <form noValidate autoComplete="off">
                         <TextField
                             id="first-name"
                             placeholder="First Name"
-                            style={{ margin: 8 }}
+                            style={{ margin: 25, width: 120 }}
                             className={TextField}
-                            value={this.state.name}
                             margin="normal"
+                            onChange={(e) => this.setState({ first_name: e.target.value })}
                         />
                         <TextField
                             id="last-name"
                             placeholder="Last Name"
-                            style={{ margin: 8 }}
+                            style={{ marginRight: 25, marginTop: 25.5, width: 120 }}
                             className={TextField}
-                            value={this.state.name}
                             margin="normal"
+                            onChange={(e) => this.setState({ last_name: e.target.value })}
                         />
                         <TextField
                             id="email-address"
                             placeholder="Your Email Address"
-                            style={{ margin: 8 }}
+                            style={{ margin: 25, marginTop: -2 }}
                             fullWidth
                             margin="normal"
                             InputLabelProps={{
                                 shrink: true
                             }}
+                            onChange={(e) => this.setState({ email: e.target.value })}
                         />
                         <TextField
                             id="password"
                             placeholder="Password"
-                            style={{ margin: 8 }}
+                            style={{ margin: 25, marginTop: -2 }}
                             fullWidth
                             margin="normal"
                             InputLabelProps={{
                                 shrink: true
                             }}
+                            type='password'
+                            onChange={(e) => this.setState({ password: e.target.value })}
                         />
                         <FormControl
-                            style={{ margin: 8 }} autoComplete="off" >
+                            style={{ marginLeft: 25, marginTop: -25 }} autoComplete="off" >
                             <InputLabel htmlFor="age-simple">Birth Month</InputLabel>
-                            <Select
-                                value={this.state.age}
-                                onChange={this.handleChange}
-                                inputProps={{
-                                    name: 'age',
-                                    id: 'age-simple',
-                                }}
-                            >
+                            <Select>
                                 <MenuItem value="">
                                     <em>None</em>
                                 </MenuItem>
-                                <MenuItem >January</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
+                                <MenuItem> January </MenuItem>
+                                <MenuItem> February </MenuItem>
+                                <MenuItem> March </MenuItem>
+                                <MenuItem> April </MenuItem>
+                                <MenuItem> May </MenuItem>
+                                <MenuItem> June </MenuItem>
+                                <MenuItem> July </MenuItem>
+                                <MenuItem> August </MenuItem>
+                                <MenuItem> September </MenuItem>
+                                <MenuItem> October </MenuItem>
+                                <MenuItem> November </MenuItem>
+                                <MenuItem> December </MenuItem>
                             </Select>
                         </FormControl>
                         <TextField
                             id="birth-day"
                             placeholder="Day"
-                            style={{ marginRight: 80, marginTop: 24, width: 70 }}
+                            style={{ marginRight: 60, marginTop: -9, width: 100 }}
                             margin="normal"
                             InputLabelProps={{
                                 shrink: true
@@ -121,13 +131,18 @@ class Signup extends Component {
                         <TextField
                             id="birth-year"
                             placeholder="year"
-                            style={{ marginLeft: -70, marginTop: 24, width: 70, marginRight: 8 }}
+                            style={{ marginLeft: -70, marginTop: -9, width: 70, marginRight: 25 }}
                             margin="normal"
                             InputLabelProps={{
                                 shrink: true
                             }}
                         />
                     </form>
+                    <h6 className='agree-to-terms' style={{ marginLeft: 55 }}> By clicking "Continue", you agree to the Terms and <p style={{ marginLeft: 90, marginBottom: -20, marginTop: 0 }}>Privacy Policy </p></h6>
+                    <div className='continue-button'>
+                        <Button variant='contained' color='primary' component={Link}  to='HomePage' style={{ width: 345, margin: 25 }} onClick={() => this.register()}> Continue </Button>
+                    </div>
+                    <h5 className='already-account'> Already have an account? <Link className='link-to-signin' to='Signin'> Sign In </Link> </h5>
                 </div>
             </div>
         )
